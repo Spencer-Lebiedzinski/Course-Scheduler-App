dd8c72b36f9141ce749449ac90707591de997926
Spencer M. Lebiedzinski <76723194+Spencer-Lebiedzinski@users.noreply.github.com>
2025-11-17 15:57:02 -0500

    Fix type safety warnings

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -39,7 +39,7 @@ public class MainFrame extends javax.swing.JFrame {
 
     public void rebuildSemesterComboBoxes() {
         ArrayList<String> semesters = SemesterQueries.getSemesterList();
-        currentSemesterComboBox.setModel(new javax.swing.DefaultComboBoxModel(semesters.toArray()));
+        currentSemesterComboBox.setModel(new javax.swing.DefaultComboBoxModel<>(semesters.toArray(new String[0])));
         if (semesters.size() > 0) {
             currentSemesterLabel.setText(semesters.get(0));
             currentSemester = semesters.get(0);
@@ -597,11 +597,11 @@ public class MainFrame extends javax.swing.JFrame {
                 "Course Code", "Description", "Seats"
             }
         ) {
-            Class[] types = new Class [] {
+            Class<?>[] types = new Class<?> [] {
                 java.lang.String.class, java.lang.String.class, java.lang.Integer.class
             };
 
-            public Class getColumnClass(int columnIndex) {
+            public Class<?> getColumnClass(int columnIndex) {
                 return types [columnIndex];
             }
         });
@@ -710,11 +710,11 @@ public class MainFrame extends javax.swing.JFrame {
                 "Course Code", "Status"
             }
         ) {
-            Class[] types = new Class [] {
+            Class<?>[] types = new Class<?> [] {
                 java.lang.String.class, java.lang.String.class
             };
 
-            public Class getColumnClass(int columnIndex) {
+            public Class<?> getColumnClass(int columnIndex) {
                 return types [columnIndex];
             }
         });
